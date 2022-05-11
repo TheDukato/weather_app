eac24169471d3757a6315e623885d9b8ed47f2c0
TheDukato <56132686+TheDukato@users.noreply.github.com>
2022-05-11 19:44:07 +0200

    Initial_commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Assumptions:
+1)handling error
+2)grafics swing, java2D,cartpanel
+3)log4j
+4)jsoup
---
+++ src/com/Study_6/proj/Main.java
@@ -0,0 +1,8 @@
+package com.Study_6.proj;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
