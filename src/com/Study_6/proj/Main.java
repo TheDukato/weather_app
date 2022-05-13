85573e57e601739e79b9fa8850f100d43c3a74a0
TheDukato <kpokusa99@gmail.com>
2022-05-13 12:51:04 +0200

    Adding basic www API in class web_API print on console date and temp value

--- src/com/Study_6/proj/Main.java
+++ src/com/Study_6/proj/Main.java
@@ -16,17 +16,8 @@ import java.util.Scanner;
 
 
 public class Main {
-
-
-
     public static void main(String[] args) {
-        //Instantiating the URL class
-        URL url = null;
-        try {
-            url = new URL("http://meteo.kdwd.webd.pl/index.php");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+
 ////////Connection to DB
         Random rand = new Random();
         DB_API.conn2DB();
@@ -35,46 +26,8 @@ public class Main {
         for(Measurement c: DB_API.selectQuerry("temperature_1"))
             System.out.println("Value: " + c.getValue());
 
-
 ////////Current temprature
-/*      String dat = "";
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        dat = dtf.format(now);
-        System.out.println("Hello: "+ dat);*/
-
-        /*while (true) {
-            if(dat!=LocalDateTime.now().toString()) {
-                //Retrieving the contents of the specified page
-                Scanner sc = null;
-                try {
-                    sc = new Scanner(url.openStream());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //Instantiating the StringBuffer class to hold the result
-                StringBuffer sb = new StringBuffer();
-                while (sc.hasNext()) {
-                    sb.append(sc.next());
-                    //System.out.println(sc.next());
-                }
-                //Retrieving the String from the String Buffer object
-                String result = sb.toString();
-                //System.out.println(result);
-                //Removing the HTML tags
-                result = result.replaceAll("<[^>]*>", "");
-                //System.out.println("Contents of the web page: "+result);
-                result = result.substring(result.indexOf("Aktualizacja:&nbsp;") + 50, result.indexOf("hrWilgotno") - 7);
-                //Format of return data: Sucho,BezchmurnieTemperatura:22.6&deg;C
-                result = result.substring(result.indexOf(":") + 1, result.indexOf("deg;C") - 1);
-                //Format of return data: 22.6
-                System.out.println("Contents of the web page: " + result);
-
-            }
-
-        }*/
+        //web_API.messyMethod();
 
     }
-
-
 }
---
+++ src/com/Study_6/proj/web_API.java
@@ -0,0 +1,71 @@
+package com.Study_6.proj;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class web_API {
+    /*messyMethod
+    *TODO
+    * 1)Delay method to use while pooling website "delay"
+     */
+    public static void messyMethod() {
+        //Instantiating the URL class
+        URL url = null;
+        try {
+            url = new URL("http://meteo.kdwd.webd.pl/index.php");
+        } catch (
+                MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        String dat = "";
+        //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        //LocalDateTime nowdate = LocalDateTime.now();
+        dat = LocalDateTime.now().toString();
+        System.out.println("Hello: "+ dat);
+
+        while (true) {
+            if (dat != LocalDateTime.now().toString()) {
+                //Retrieving the contents of the specified page
+                Scanner sc = null;
+                try {
+                    sc = new Scanner(url.openStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //Instantiating the StringBuffer class to hold the result
+                StringBuffer sb = new StringBuffer();
+                while (sc.hasNext()) {
+                    sb.append(sc.next());
+                    //System.out.println(sc.next());
+                }
+                //Retrieving the String from the String Buffer object
+                String result = sb.toString();
+                //Removing the HTML tags
+                result = result.replaceAll("<[^>]*>", "");
+                //System.out.println("Contents of the web page: "+result);
+                result = result.substring(result.indexOf("Aktualizacja:&nbsp;") + 50, result.indexOf("hrWilgotno") - 7);
+                //Format of return data: Sucho,BezchmurnieTemperatura:22.6&deg;C
+                result = result.substring(result.indexOf(":") + 1, result.indexOf("deg;C") - 1);
+                //Format of return data: 22.6
+                System.out.println("Contents of the web page: " + result+"\nAt time: "+ LocalDateTime.now().toString());
+
+
+                web_API.delay(10000);
+
+            }
+        }
+    }
+    private static boolean delay(int delTime){
+        ///Periodic issue
+        for(int i = 0; i< delTime;i++){
+            //do Something
+        }
+        return true;
+    }
+
+}
